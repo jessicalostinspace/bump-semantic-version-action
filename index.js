362ef6709a82ead68b31f913d4d283acb1ffbde4
Jessica Wilson <jwilson@zipwhip.com>
2020-01-06 09:35:13 -0800

    Added initial index and inputs and output from previous workflow step

---
+++ index.js
@@ -0,0 +1,13 @@
+const core = require('@actions/core');
+
+try {
+  // `who-to-greet` input defined in action metadata file
+  const semanticVersion = core.getInput('semantic-version');
+  const versionType = core.getInput('version-type');
+  console.log("semanticVer: ", semanticVersion);
+  console.log("version Type: ", versionType);
+
+  //   core.setOutput("time", time);
+} catch (error) {
+  core.setFailed(error.message);
+}

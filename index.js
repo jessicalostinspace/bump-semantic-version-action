4bbaaf4d76746153d5df27ebcb4969838a798a4b
Jessica Wilson <jwilson@zipwhip.com>
2020-01-06 15:01:17 -0800

    Refactored

--- index.js
+++ index.js
@@ -8,27 +8,24 @@ try {
   let minorNumber = semanticVersion.toString().split(".")[1];
   let patchNumber = semanticVersion.toString().split(".")[2];
 
-  let newSemanticVersion = majorNumber + "." + minorNumber + "." + patchNumber;
+  const newSemanticVersion = majorNumber + "." + minorNumber + "." + patchNumber;
   switch(versionType) {
     case 'MAJOR':
       majorNumber = parseInt(majorNumber, 10) + 1;
-      newSemanticVersion = majorNumber + "." + minorNumber + "." + patchNumber;
-      core.setOutput("bumped-semantic-version", newSemanticVersion);
       break;
     case 'MINOR':
       minorNumber = parseInt(minorNumber, 10) + 1;
-      newSemanticVersion = majorNumber + "." + minorNumber + "." + patchNumber;
-      core.setOutput("bumped-semantic-version", newSemanticVersion);
-      console.log("newSemVer: ", newSemanticVersion)
       break;
     case 'PATCH':
       patchNumber = parseInt(patchNumber, 10) + 1;
-      newSemanticVersion = majorNumber + "." + minorNumber + "." + patchNumber;
-      core.setOutput("bumped-semantic-version", newSemanticVersion);
       break;
     default:
       core.setFailed("Could not process the version type. Make sure your string is either 'MAJOR', 'MINOR', or 'PATCH'.");
   }
+
+  const newSemanticVersion = majorNumber + "." + minorNumber + "." + patchNumber;
+  core.setOutput("bumped-semantic-version", newSemanticVersion);
+  console.log('\x1b[32m%s\x1b[0m', `Bumped Semantic Version: ${newSemanticVersion}`);
 } catch (error) {
   core.setFailed(error.message);
 }

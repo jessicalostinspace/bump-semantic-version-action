20decd9805c12959e16e4b861ec009bee0dcd911
Jessica <jessicalostinspace@gmail.com>
2020-01-06 15:03:21 -0800

    Development (#1)
    * Added initial action metadata file
    
    * Added initial index and inputs and output from previous workflow step
    
    * Adding marketplace action
    
    * Bumped previous action version
    
    * Added copied github bash script
    
    * Debugging first github action
    
    * Moving bash file temporarily
    
    * Checking for major number
    
    * Split version into major,minor,patch
    
    * Forced uppercase version type
    
    * Bumped version number
    
    * Removing const
    
    * Convert string to num
    
    * Removed logs
    
    * Refactored
    
    * Removed dupe declaration

---
+++ index.js
@@ -0,0 +1,30 @@
+const core = require('@actions/core');
+
+try {
+  const semanticVersion = core.getInput('semantic-version');
+  const versionType = core.getInput('version-type').toUpperCase();
+
+  let majorNumber = semanticVersion.toString().split(".")[0];
+  let minorNumber = semanticVersion.toString().split(".")[1];
+  let patchNumber = semanticVersion.toString().split(".")[2];
+
+  switch(versionType) {
+    case 'MAJOR':
+      majorNumber = parseInt(majorNumber, 10) + 1;
+      break;
+    case 'MINOR':
+      minorNumber = parseInt(minorNumber, 10) + 1;
+      break;
+    case 'PATCH':
+      patchNumber = parseInt(patchNumber, 10) + 1;
+      break;
+    default:
+      core.setFailed("Could not process the version type. Make sure your string is either 'MAJOR', 'MINOR', or 'PATCH'.");
+  }
+
+  const newSemanticVersion = majorNumber + "." + minorNumber + "." + patchNumber;
+  core.setOutput("bumped-semantic-version", newSemanticVersion);
+  console.log('\x1b[32m%s\x1b[0m', `Bumped Semantic Version: ${newSemanticVersion}`);
+} catch (error) {
+  core.setFailed(error.message);
+}

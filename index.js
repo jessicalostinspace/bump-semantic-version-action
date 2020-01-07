b915bfb0a53965f8206898d80ca6a9b5efb6817f
Jessica <jessicalostinspace@gmail.com>
2020-01-07 11:32:48 -0800

    Development (#2)
    * Fixed versioning to refelect when 0's reset correctly

--- index.js
+++ index.js
@@ -8,21 +8,26 @@ try {
   let minorNumber = semanticVersion.toString().split(".")[1];
   let patchNumber = semanticVersion.toString().split(".")[2];
 
+  let newSemanticVersion = '';
+
   switch(versionType) {
     case 'MAJOR':
       majorNumber = parseInt(majorNumber, 10) + 1;
+      newSemanticVersion = majorNumber + ".0.0";
       break;
     case 'MINOR':
       minorNumber = parseInt(minorNumber, 10) + 1;
+      newSemanticVersion = majorNumber + "." + minorNumber + ".0"
       break;
     case 'PATCH':
       patchNumber = parseInt(patchNumber, 10) + 1;
+      newSemanticVersion = majorNumber + "." + minorNumber + "." + patchNumber;
+
       break;
     default:
       core.setFailed("Could not process the version type. Make sure your string is either 'MAJOR', 'MINOR', or 'PATCH'.");
   }
 
-  const newSemanticVersion = majorNumber + "." + minorNumber + "." + patchNumber;
   core.setOutput("bumped-semantic-version", newSemanticVersion);
   console.log('\x1b[32m%s\x1b[0m', `Bumped Semantic Version: ${newSemanticVersion}`);
 } catch (error) {
